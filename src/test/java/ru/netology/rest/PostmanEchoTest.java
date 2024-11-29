0bea5ddbb596ec3772e76cc18d27ff7b5ac15357
Tatiana <ef.tatiana@mail.ru>
2024-11-30 00:46:07 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    void ShouldReturnCreatedData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world")
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data",org.hamcrest.Matchers.equalTo("Hello"));
+    }
+
+}
